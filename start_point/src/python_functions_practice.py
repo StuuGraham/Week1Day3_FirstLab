7f6744f42851c00b7571d88bdfc50e6cc9e981ab
StuuGraham <stuartgraham7@hotmail.co.uk>
2020-10-14 12:43:29 +0100

    Tidying up of indenting at end of Lab session

--- start_point/src/python_functions_practice.py
+++ start_point/src/python_functions_practice.py
@@ -26,6 +26,33 @@ def add_string_as_number(string_1, string_2):
       string_1 = "1"
       string_2 = "2"
       return int(string_1) + int(string_2)
-     
-     
+
+
+def number_to_full_month_name(month_number):
+    if month_number == 1:
+        return "January"
+
+    elif month_number == 3:
+        return "March"
+
+    elif month_number == 9:
+        return "September"
+
+def number_to_short_month_name(month_number):
+    if month_number == 1:
+        return "Jan"
+    
+    elif month_number == 4:
+        return "Apr"
+
+    elif month_number == 10:
+        return "Oct"
+
+
+def volume_of_cube(l, b, h):
+    volume = l * b * h
+    return volume
+    
+
+
       
--- start_point/tests/python_functions_test.py
+++ start_point/tests/python_functions_test.py
@@ -40,32 +40,26 @@ class TestPythonFunctionPractice(unittest.TestCase):
       add_result = add_string_as_number( "1", "2" )
       self.assertEqual( 3, add_result )
 
-  @unittest.skip("delete this line to run the test")
   def test_number_to_full_name__month_1(self):
       result = number_to_full_month_name( 1 )
       self.assertEqual( "January", result )
 
-  @unittest.skip("delete this line to run the test")
   def test_number_to_full_name__month_3(self):
       result = number_to_full_month_name( 3 )
       self.assertEqual( "March", result )
 
-  @unittest.skip("delete this line to run the test")
   def test_number_to_full_name__month_9(self):
       result = number_to_full_month_name( 9 )
       self.assertEqual( "September", result )
 
-  @unittest.skip("delete this line to run the test")
   def test_number_to_short_month_name__month_1(self):
       first_month_string = number_to_short_month_name( 1 )
       self.assertEqual( "Jan", first_month_string )
 
-  @unittest.skip("delete this line to run the test")
   def test_number_to_short_month_name__month_4(self):
       fourth_month_string = number_to_short_month_name( 4 )
       self.assertEqual( "Apr", fourth_month_string )
 
-  @unittest.skip("delete this line to run the test")
   def test_number_to_short_month_name__month_10(self):
       tenth_month_string = number_to_short_month_name( 10 )
       self.assertEqual( "Oct", tenth_month_string )
@@ -74,8 +68,10 @@ class TestPythonFunctionPractice(unittest.TestCase):
 
   #Given the length of a side of a cube calculate the volume
   @unittest.skip("delete this line to run the test")
-  def test_volume_of_cube(self):
-    #add test code here
+  
+def volume_of_cube(l, b, h):
+    volume = l * b * h
+    return volume
     pass
 
   #Given a String, return the String reversed
