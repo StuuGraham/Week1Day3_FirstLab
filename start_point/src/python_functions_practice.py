6d11e09d26d4772fcebc4e1ef67e9a79304b7545
StuuGraham <stuartgraham7@hotmail.co.uk>
2020-10-14 11:58:44 +0100

    Completed 8 Tests

--- start_point/src/python_functions_practice.py
+++ start_point/src/python_functions_practice.py
@@ -1,2 +1,31 @@
 def return_10():
     return 10
+
+def add(num_1, num_2):
+    return num_1 + num_2
+
+def subtract(num_1, num_2):
+    return num_1 - num_2
+
+def multiply(num_1, num_2):
+    return num_1 * num_2
+
+def divide(num_1, num_2):
+    return num_1 / num_2
+
+def length_of_string(self):
+      string = "A string of length 21"
+      return len(string)
+
+def join_string(string_1, string_2):
+      string_1 = "Mary had a little lamb, "
+      string_2 = "its fleece was white as snow"
+      return string_1 + string_2
+
+def add_string_as_number(string_1, string_2):
+      string_1 = "1"
+      string_2 = "2"
+      return int(string_1) + int(string_2)
+     
+     
+      
